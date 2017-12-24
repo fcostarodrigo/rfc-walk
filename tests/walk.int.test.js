5298e62204e5f09b30aee7d669594eb85f3bd23c
Rodrigo Fernandes da Costa <fcostarodrigo@yahoo.com>
2017-12-24 14:22:11 -0200

    Add integration test

--- tests/walk.int.test.js
+++ tests/walk.int.test.js
@@ -1,5 +1,18 @@
+const walk = require("../.");
+
 describe("walk", () => {
-  it("should walk the sample folder recursively", () => {
-    expect(true).toBe(true);
+  it("should walk the sample folder recursively", async () => {
+    const paths = await walk({ root: "tests/a", includeFolders: true });
+    expect(paths.sort()).toEqual([
+      "tests/a",
+      "tests/a/b",
+      "tests/a/b/g",
+      "tests/a/b/h",
+      "tests/a/c",
+      "tests/a/c/d",
+      "tests/a/c/d/e",
+      "tests/a/c/f",
+      "tests/a/i"
+    ]);
   });
 });
