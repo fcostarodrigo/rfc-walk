1aa6fcadd1d74c4ee4539699e45d9e934aa19c62
Rodrigo Fernandes da Costa <fcostarodrigo@yahoo.com>
2017-12-24 16:05:45 -0200

    Refactor

--- src/index.unit.test.js
+++ src/index.unit.test.js
@@ -3,141 +3,74 @@ const walk = require("./index");
 
 jest.mock("fs");
 
+const mockReadDir = results => {
+  for (const { folder: expectedFolder, result } of results) {
+    fs.readdir.mockImplementationOnce((folder, callback) => {
+      expect(folder).toBe(expectedFolder);
+      callback(...result);
+    });
+  }
+};
+
 describe("walk", () => {
+  beforeAll(() => {
+    jest.resetAllMocks();
+  });
+
   it("should transverse files recursively", async () => {
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe(".");
-      callback(null, ["a", "b", "c"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("a");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("b");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c");
-      callback(null, ["d", "e"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c/d");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c/e");
-      callback({ code: "ENOTDIR" });
-    });
+    mockReadDir([
+      { folder: ".", result: [null, ["a", "b", "c"]] },
+      { folder: "a", result: [{ code: "ENOTDIR" }] },
+      { folder: "b", result: [{ code: "ENOTDIR" }] },
+      { folder: "c", result: [null, ["d", "e"]] },
+      { folder: "c/d", result: [{ code: "ENOTDIR" }] },
+      { folder: "c/e", result: [{ code: "ENOTDIR" }] }
+    ]);
 
     expect(await walk()).toEqual(["a", "b", "c/d", "c/e"]);
   });
 
   it("should propagate errors", () => {
     const error = new Error("error");
+
     fs.readdir.mockImplementationOnce(() => {
       throw error;
     });
+
     return expect(walk()).rejects.toBe(error);
   });
 
   it("should transverse starting in a folder", async () => {
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("a/a");
-      callback(null, ["a"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("a/a/a");
-      callback({ code: "ENOTDIR" });
-    });
+    mockReadDir([
+      { folder: "a/a", result: [null, ["a"]] },
+      { folder: "a/a/a", result: [{ code: "ENOTDIR" }] }
+    ]);
+
     expect(await walk({ root: "a/a" })).toEqual(["a/a/a"]);
   });
 
   it("should list folders", async () => {
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe(".");
-      callback(null, ["a"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("a");
-      callback({ code: "ENOTDIR" });
-    });
+    mockReadDir([
+      { folder: ".", result: [null, ["a"]] },
+      { folder: "a", result: [{ code: "ENOTDIR" }] }
+    ]);
+
     expect(await walk({ includeFolders: true })).toEqual([".", "a"]);
   });
 
   it("should call onPath for each file", async () => {
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe(".");
-      callback(null, ["a", "b", "c"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("a");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("b");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c");
-      callback(null, ["d", "e"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c/d");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c/e");
-      callback({ code: "ENOTDIR" });
-    });
+    mockReadDir([
+      { folder: ".", result: [null, ["a", "c"]] },
+      { folder: "a", result: [{ code: "ENOTDIR" }] },
+      { folder: "c", result: [null, ["d"]] },
+      { folder: "c/d", result: [{ code: "ENOTDIR" }] }
+    ]);
 
     const onPath = jest.fn();
 
     await walk({ onPath });
 
     expect(onPath.mock.calls[0][0]).toBe("a");
-    expect(onPath.mock.calls[1][0]).toBe("b");
-    expect(onPath.mock.calls[2][0]).toBe("c/d");
-    expect(onPath.mock.calls[3][0]).toBe("c/e");
-  });
-
-  it("should wait for all callbacks to resolve", async () => {
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe(".");
-      callback(null, ["a", "b", "c"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("a");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("b");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c");
-      callback(null, ["d", "e"]);
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c/d");
-      callback({ code: "ENOTDIR" });
-    });
-    fs.readdir.mockImplementationOnce((folder, callback) => {
-      expect(folder).toBe("c/e");
-      callback({ code: "ENOTDIR" });
-    });
-
-    const files = [];
-    const onPath = file =>
-      new Promise(resolve =>
-        setTimeout(() => {
-          files.push(file);
-          resolve();
-        })
-      );
-
-    await walk({ onPath });
-
-    expect(files).toEqual(["a", "b", "c/d", "c/e"]);
+    expect(onPath.mock.calls[1][0]).toBe("c/d");
   });
 });
--- tests/walk.int.test.js
+++ tests/walk.int.test.js
@@ -1,18 +1,43 @@
 const walk = require("../.");
 
+const expectedPaths = [
+  "tests/sampleToTransverse",
+  "tests/sampleToTransverse/b",
+  "tests/sampleToTransverse/b/g",
+  "tests/sampleToTransverse/b/h",
+  "tests/sampleToTransverse/c",
+  "tests/sampleToTransverse/c/d",
+  "tests/sampleToTransverse/c/d/e",
+  "tests/sampleToTransverse/c/f",
+  "tests/sampleToTransverse/i"
+];
+
 describe("walk", () => {
   it("should walk the sample folder recursively", async () => {
-    const paths = await walk({ root: "tests/a", includeFolders: true });
-    expect(paths.sort()).toEqual([
-      "tests/a",
-      "tests/a/b",
-      "tests/a/b/g",
-      "tests/a/b/h",
-      "tests/a/c",
-      "tests/a/c/d",
-      "tests/a/c/d/e",
-      "tests/a/c/f",
-      "tests/a/i"
-    ]);
+    const paths = await walk({
+      root: "tests/sampleToTransverse",
+      includeFolders: true
+    });
+
+    expect(paths.sort()).toEqual(expectedPaths);
+  });
+
+  it("should wait for all promises returned by onPath to resolve", async () => {
+    const paths = [];
+    const onPath = path =>
+      new Promise(resolve =>
+        setTimeout(() => {
+          paths.push(path);
+          resolve();
+        }, 100)
+      );
+
+    await walk({
+      onPath,
+      root: "tests/sampleToTransverse",
+      includeFolders: true
+    });
+
+    expect(paths.sort()).toEqual(expectedPaths);
   });
 });
